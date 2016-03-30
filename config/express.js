5d0d4091c820635ebaf755820ac17ff2ac8baf94
Byrdann Fox <byrdannfox@gmail.com>
2016-03-30 18:58:01 -0400

    my mac mini icloud ruined my development groove, I'm sad now.

--- config/express.js
+++ config/express.js
@@ -9,11 +9,7 @@ module.exports = function() {
 	return app;
 };
 
-
-/* ### Other information and guidelines:
- *
- * - Use the node version specified by the engines property in package.json.
- *
+/*
  * - `url` should include the protocol and domain, which would be www.cnn.com.
  *
  * - Not all articles will have an image.  If an article has something like a
@@ -21,17 +17,7 @@ module.exports = function() {
  *
  * - The `imageUrl` should reference the highest quality image available.
  *
- * - The `byLine` should contain information about the author of the article,
- *   which may not be available for every article.
- *
- * - The source feed will represent what is on the top of the www.cnn.com
- *   homepage.  The content to map is in a _container_ with a _title_ of
- *   "Top stories".  This content changes all the time.  The placement of
- *   this container will change as well.
- *
- *
- *
- *
+
  * ## Task 2 (of 2)
  *
  * Configure the project to execute ESLint on all JavaScript files in the
