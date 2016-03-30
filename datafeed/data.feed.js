217b82109385d025d4f45148d465e079e8aff802
Ryan Poplin <ryanpoplin@Ryans-Mac-mini.local>
2016-03-30 15:13:04 -0400

    notes on JSON

--- datafeed/data.feed.js
+++ datafeed/data.feed.js
@@ -4,10 +4,8 @@ module.exports = function() {
 	const request = require('superagent');
 	const dataUrl = 'http://www.cnn.com/data/ocs/section/index.html:homepage1-zone-1.json';
 	request.get(dataUrl).end(function(err, res) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log(JSON.stringify(res.body));
-		}
+		(err) ? console.log(err) : console.log([JSON.stringify(res.body)]);
+		// run a method here to transform the data accordingly, invoke in the ternary above.
+		// this will do all the things and log to the console the [{}, {}, {}] an array of article objects...
 	});
 };
