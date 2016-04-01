edf79d6f5ee78ebeec1e18358be8ba41f75ffeee
Byrdann Fox <byrdannfox@gmail.com>
2016-04-01 16:19:35 -0400

    adding es6 support bc I like this as a boiler plate project

--- config/express.js
+++ config/express.js
@@ -1,12 +1,17 @@
 'use strict';
 
-const express = require('express');
+import express from 'express';
+
 module.exports = function () {
     const app = express();
 
-    require('../datafeed/data.feed')(function (dataFeed) {
-        console.log(JSON.stringify(dataFeed));
-    });
+    // TODO: move routes into a routes file within a routes directory
+    app.get('/', (req, res) =>
+    res.json({status: 'NTask API'}));
+
+    // require('../datafeed/data.feed')(function (dataFeed) {
+    //     console.log(JSON.stringify(dataFeed));
+    // });
 
     return app;
 };
--- main.js
+++ main.js
@@ -4,4 +4,5 @@ const config = require('./config/config'),
     express = require('./config/express'),
     app = express();
 
-app.listen(config.port, config.ip);
+app.listen(config.port, config.ip, () =>
+console.log(`NTask API - Port ${config.port}`));
