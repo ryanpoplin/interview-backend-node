9aa7be57a1fc1a6b28136b5766effe1a7f4ef0d9
Byrdann Fox <byrdannfox@gmail.com>
2016-04-13 18:30:20 -0400

    asdfjkl;

--- regex/regex.test.js
+++ regex/regex.test.js
@@ -5,9 +5,12 @@ const _ = require('lodash');
 function getMatches (rgx, text) {
 	const deep = _.cloneDeep([rgx]),
 		rgxClone = deep[0],
-		results = [];
+		results = [],
+		resultsContainer = {};
+	
 	var match, 
 		i;
+
 	while (rgx.exec(text) !== null) {
 		match = rgxClone.exec(text);
 		if (match.index > i) {
@@ -20,23 +23,26 @@ function getMatches (rgx, text) {
 			break;
 		}
 	}
-	console.log(results);
-	return results;
-}
 
-function getMatchesCountString (results) {
 	if (results.length > 0) {
-		console.log(`There are ${results.length} matches`);
+		resultsContainer.matches = `There are ${results.length} matches`;
 	} else {
-		console.log('There are 0 matches');
+		resultsContainer.matches = 'There are 0 matches';
 	}
+
+	resultsContainer.results = results;
+	console.log(resultsContainer);
+	return resultsContainer;
 }
 
-const matches = getMatches(/^(?:https?:\/\/)?\w+(?:\.\w+)?(?:\.[A-Z]{2,7})+$/i, 'https://www.luupy.io');
-getMatchesCountString(matches);
+// module.exports = {
+// 	getMatches: getMatches
+// };
 
-const matches2 = getMatches(/aa  a   /, 'aa  a   ');
-getMatchesCountString(matches2);
-
-const matches3 = getMatches(/Here we go regular expressions /, 'Here we go regular expressions');
-getMatchesCountString(matches3);
+// getMatches(/^(?:https?:\/\/)?\w+(?:\.\w+)?(?:\.[A-Z]{2,7})+$/i, 'https://www.luupy.io');
+// getMatches(/aa  a        /g, 'aa  a        aa  a        ');
+// getMatches(/Here we go regular expressions /, 'Here we go regular expressions ');
+// getMatches(/1.3/g, '1 3183');
+// getMatches(/1\d3/g, '123123123123');
+// getMatches(/1\w3/g, '1b2');
+getMatches(/a a  a/, 'a a  a');
