1e8ad1084827012c155585830a713f63eb4c5138
Byrdann Fox <byrdannfox@gmail.com>
2016-04-14 17:27:05 -0400

    asdfjkl;

--- regex/regex.test.js
+++ regex/regex.test.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const _ = require('lodash');
-
-function getMatches (rgx, text) {
-	const deep = _.cloneDeep([rgx]),
+function getMatchData (rgx, text) {
+	const _ = require('lodash'),
+		deep = _.cloneDeep([rgx]),
 		rgxClone = deep[0],
 		results = [],
 		resultsContainer = {};
@@ -22,23 +21,28 @@ function getMatches (rgx, text) {
 			break;
 		}
 	}
+	resultsContainer.results = results;
 
-	if (results.length > 0) {
-		// TODO: refactor for proper string grammer return
-		resultsContainer.matches = `There are ${results.length} matches`;
-	} else {
-		resultsContainer.matches = 'There are 0 matches';
+	switch (results.length) {
+		case 0:
+			resultsContainer.matchInfo = 'There are 0 matches';
+			break;
+		case 1:
+			resultsContainer.matchInfo = 'There is 1 match.';	
+			break;
+		default:
+			resultsContainer.matchInfo = `There are ${results.length} matches`;
+			break;
 	}
 
-	resultsContainer.results = results;
 	console.log(resultsContainer);
 	return resultsContainer;
 }
 
+getMatchData(/a a  a/gi, 'a a  aa a  AA A  a');
 // getMatches(/^(?:https?:\/\/)?\w+(?:\.\w+)?(?:\.[A-Z]{2,7})+$/i, 'https://www.luupy.io');
 // getMatches(/aa  a        /g, 'aa  a        aa  a        ');
 // getMatches(/Here we go regular expressions /, 'Here we go regular expressions ');
 // getMatches(/1.3/g, '1 3183');
 // getMatches(/1\d3/g, '123123123123');
 // getMatches(/1\w3/g, '1b2');
-getMatches(/a a  a/, 'a a  aa a  A');
